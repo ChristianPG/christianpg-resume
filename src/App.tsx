da9bcc5065745c4ff3ed2d2f93b293ed47f0c656
Christian Patiño <christianpg813@gmail.com>
2025-07-24 07:00:55 -0500

    Update the initial content and remove the custom base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,18 +13,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Christian Patiño</h1>
+      <h2>Senior Frontend Developer</h2>
+      <h3>WIP</h3>
+      <a href="https://www.linkedin.com/in/christianpg" target="_blank">
+        LinkedIn Profile
+      </a>
     </>
   )
 }
